7eba565ff38e2c3477f17c4f4b8009ac28f0db1b
myplacecoffee <74812757+myplacecoffee@users.noreply.github.com>
2022-09-01 17:15:51 +0900

    before server

--- src/app.jsx
+++ src/app.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import {StreamChat} from 'stream-chat';
 import {Chat} from 'stream-chat-react';
 import Cookies from 'universal-cookie';
-import {ChannelContainer, ChannelListContainer} from './components';
+import {ChannelContainer, ChannelListContainer, Auth} from './components';
 import './App.css';
 
-const apiKey='kksfn58xag79';
-const client=StreamChat.getInstance(apiKey);
+const client=StreamChat.getInstance(process.env.REACT_APP_API_KEY);
+
+const authToken=false;
 
 function App() {
+  if(!authToken) return <Auth />
+
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export {default as ChannelListContainer} from './channelListContainer'
 export {default as ChannelSearch} from './channelSearch';
 export {default as TeamChannelList} from './teamChannelList';
 export {default as TeamChannelPreview} from './teamChannelPreview';
+export {default as Auth} from './auth';
